e18b92608eb23d5fdf82a508a14ed7b94274c44e
gabrielmariani <gabrielmfanez@gmail.com>
2025-11-21 21:02:23 -0300

    Update Criador.java

---
+++ src/pessoas/Criador.java
@@ -0,0 +1,13 @@
+package pessoas;
+    
+public class Criador extends Pessoa{
+    private String biografia;
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+}
