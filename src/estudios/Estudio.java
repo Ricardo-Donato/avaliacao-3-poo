124e52678bd176d62a7dc40fcd65241298a2cfb8
Ricardo-Donato <pdonatodasilva@gmail.com>
2025-11-21 21:09:17 -0300

    Implementação da classe Estudio

--- src/estudios/Estudio.java
+++ src/estudios/Estudio.java
@@ -1,1 +1,38 @@
+package estudios;
 
+import java.util.List;
+import midias.Midia;
+
+public class Estudio {
+    private int id;
+    private String nome;
+    private List<Midia> publicacoes;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPublicacoes(List<Midia> publicacoes) {
+        this.publicacoes = publicacoes;
+    }
+    
+    public List<Midia> getPublicacoes() {
+        return publicacoes;
+    }
+    
+    public void publicar(Midia midia) {
+        this.publicacoes.add(midia);
+    }
+}
